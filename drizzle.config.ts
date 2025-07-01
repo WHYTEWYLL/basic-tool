98f385822d3154159337255ce9f5051b70c52e12
WHYTEWYLL <juanmiblanco1@gmail.com>
2025-07-01 15:17:29 +0200

    tool test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "drizzle-kit";
-import { env } from "@/lib/env.mjs";
 
 export default {
   schema: "./lib/db/schema",
   dialect: "postgresql",
   out: "./lib/db/migrations",
   dbCredentials: {
-    url: env.VECTOR_DATABASE_URL,
-  }
+    url: process.env.VECTOR_DATABASE_URL!,
+  },
 } satisfies Config;
